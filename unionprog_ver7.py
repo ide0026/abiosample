bc5717a0ca20a566ad87754ea2185ca6e698e5f1
ide0026 <95006179+ide0026@users.noreply.github.com>
2022-12-05 09:21:28 +0900

    Update unionprog_ver7.py

--- unionprog_ver7.py
+++ unionprog_ver7.py
@@ -14,7 +14,7 @@ layout_Ondo = go.Layout(title=dict(text='<b>【温度】'),
                     x=0.32,
                     y=1.0,
                     orientation='h'    
-                    ),width=1500,height=500)
+                    ),width=1400,height=700)
 layout_Situdo = go.Layout(title=dict(text='<b>【相対湿度】'),
                     yaxis = dict(side = 'left',range = [0, 110]),
                     font=dict(size=15),
