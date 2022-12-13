0026a85488f43d6150c2c2559dadf6ad2e244562
ide0026 <95006179+ide0026@users.noreply.github.com>
2022-12-13 09:24:24 +0900

    Update unionprog_ver9.4.py

--- unionprog_ver9.4.py
+++ unionprog_ver9.4.py
@@ -942,12 +942,12 @@ if uploaded_file1 and uploaded_file2 and uploaded_file3 and '4.複数ファイ�
     stocks = st.sidebar.selectbox(label="温室番号の選択",
                 options = listnum)
     select_onsitu = int(stocks[0])
-    def ex0():
-        return df_readfile1[df_readfile1["温室"] == select_onsitu]
-    def ex1():
-        return df_readfile2[df_readfile2["温室"] == select_onsitu]
-    def ex2():
-        return df_readfile3[df_readfile3["温室"] == select_onsitu]
+#     def ex0():
+#         return df_readfile1[df_readfile1["温室"] == select_onsitu]
+#     def ex1():
+#         return df_readfile2[df_readfile2["温室"] == select_onsitu]
+#     def ex2():
+#         return df_readfile3[df_readfile3["温室"] == select_onsitu]
     # stocks_file = st.selectbox("ファイルの順番を指定する",options=["①,②,③","①,③,②","②,①,③","②,③,①","③,①,②","③,②,①"])
     # if stocks_file == "①,②,③":
     #     df_ex0 = ex0()
@@ -973,21 +973,21 @@ if uploaded_file1 and uploaded_file2 and uploaded_file3 and '4.複数ファイ�
     #     df_ex2 = ex0()
     #     df_ex1 = ex1()
     #     df_ex0 = ex2()
-#     if st.sidebar.checkbox("ファイル①の月日を選択する"):
-#         select_dates = st.sidebar.date_input('①表示日付の選択',value=(df_readfile1.index[0],df_readfile1.index[-1]),min_value=df_readfile1.index[0],max_value=df_readfile1.index[-1])
-#         df_readfile1 = df_readfile1[select_dates[0].strftime("%Y-%m-%d"):select_dates[-1].strftime("%Y-%m-%d")]
-#     if st.sidebar.checkbox("ファイル②の月日を選択する"):
-#         select_dates2 = st.sidebar.date_input('②表示日付の選択',value=(df_readfile2.index[0],df_readfile2.index[-1]),min_value=df_readfile2.index[0],max_value=df_readfile2.index[-1])
-#         df_readfile2 = df_readfile2[select_dates2[0].strftime("%Y-%m-%d"):select_dates2[-1].strftime("%Y-%m-%d")]
-#     if st.sidebar.checkbox("ファイル③の月日を選択する"):
-#         select_dates3 = st.sidebar.date_input('③表示日付の選択',value=(df_readfile3.index[0],df_readfile3.index[-1]),min_value=df_readfile3.index[0],max_value=df_readfile3.index[-1])
-#         df_readfile3 = df_readfile3[select_dates3[0].strftime("%Y-%m-%d"):select_dates3[-1].strftime("%Y-%m-%d")]
-#     def ex0():
-#         return df_readfile1[df_readfile1["温室"] == select_onsitu]
-#     def ex1():
-#         return df_readfile2[df_readfile2["温室"] == select_onsitu]
-#     def ex2():
-#         return df_readfile3[df_readfile3["温室"] == select_onsitu]
+    if st.sidebar.checkbox("ファイル①の月日を選択する"):
+        select_dates = st.sidebar.date_input('①表示日付の選択',value=(df_readfile1.index[0],df_readfile1.index[-1]),min_value=df_readfile1.index[0],max_value=df_readfile1.index[-1])
+        df_readfile1 = df_readfile1[select_dates[0].strftime("%Y-%m-%d"):select_dates[-1].strftime("%Y-%m-%d")]
+    if st.sidebar.checkbox("ファイル②の月日を選択する"):
+        select_dates2 = st.sidebar.date_input('②表示日付の選択',value=(df_readfile2.index[0],df_readfile2.index[-1]),min_value=df_readfile2.index[0],max_value=df_readfile2.index[-1])
+        df_readfile2 = df_readfile2[select_dates2[0].strftime("%Y-%m-%d"):select_dates2[-1].strftime("%Y-%m-%d")]
+    if st.sidebar.checkbox("ファイル③の月日を選択する"):
+        select_dates3 = st.sidebar.date_input('③表示日付の選択',value=(df_readfile3.index[0],df_readfile3.index[-1]),min_value=df_readfile3.index[0],max_value=df_readfile3.index[-1])
+        df_readfile3 = df_readfile3[select_dates3[0].strftime("%Y-%m-%d"):select_dates3[-1].strftime("%Y-%m-%d")]
+    def ex0():
+        return df_readfile1[df_readfile1["温室"] == select_onsitu]
+    def ex1():
+        return df_readfile2[df_readfile2["温室"] == select_onsitu]
+    def ex2():
+        return df_readfile3[df_readfile3["温室"] == select_onsitu]
 #     stocks_file = st.selectbox("ファイルの順番を指定する",options=["①,②,③","①,③,②","②,①,③","②,③,①","③,①,②","③,②,①"])
 #     if stocks_file == "①,②,③":
 #         df_ex0 = ex0()
